ef94e413c54762579cff99436b8d23548fa7db79
Salvydas Lukosius <sall@w-ss.io>
2021-12-11 12:23:28 +0000

    modified:   .editorconfig 	modified:   z-a-default-ice.plugin.zsh 	renamed:    functions/za-default-ice-main-cmd -> za-default-ice-main-cmd 	renamed:    functions/za-default-ice-main-cmd-help-handler -> za-default-ice-main-cmd-help-handler 	renamed:    functions/za-default-ice-preinit-handler -> za-default-ice-preinit-handler 	renamed:    functions/za-default-ice-show-default-ices -> za-default-ice-show-default-ices 	renamed:    functions/za-default-ice-stats -> za-default-ice-stats

--- z-a-default-ice.plugin.zsh
+++ z-a-default-ice.plugin.zsh
@@ -7,38 +7,30 @@
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
-# According to the Zsh Plugin Standard function directory.
-# https://github.com/z-shell/zi/wiki/Zsh-Plugin-Standard#2-functions-directory
-
-if [[ $PMSPEC != *f* ]] {
-  fpath+=( "${0:h}/functions" )
-}
-
 # Standard hash for plugins:
 # https://github.com/z-shell/zi/wiki/Zsh-Plugin-Standard#standard-plugins-hash
 typeset -gA Plugins
 Plugins[DEFAULT_ICE_DIR]="${0:h}"
 
 autoload -Uz za-default-ice-preinit-handler \
-  za-default-ice-main-cmd-help-handler \
-  za-default-ice-main-cmd \
-  za-default-ice-show-default-ices \
-  za-default-ice-stats
+za-default-ice-main-cmd-help-handler \
+za-default-ice-main-cmd \
+za-default-ice-show-default-ices \
+za-default-ice-stats
 
 # An empty stub to fill the help handler fields
 za-default-ice-help-null-handler() { :; }
 
 # The unscoping-support hook.
-@zi-register-annex "z-a-default-ice" \
-  hook:preinit-3 \
-  za-default-ice-preinit-handler \
-  za-default-ice-help-null-handler
+@zi-register-annex "z-a-default-ice" hook:preinit-3 \
+za-default-ice-preinit-handler \
+za-default-ice-help-null-handler
 
 # The subcommand `meta'.
 @zi-register-annex "z-a-default-ice" \
-  subcommand:default-ice \
-  za-default-ice-main-cmd \
-  za-default-ice-main-cmd-help-handler
+subcommand:default-ice \
+za-default-ice-main-cmd \
+za-default-ice-main-cmd-help-handler
 
 (( Plugins[DEFAULT_ICE_stats:existing-ices] = 0 ))
 (( Plugins[DEFAULT_ICE_stats:executions] = 0 ))
